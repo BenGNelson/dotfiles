8919d6b7fe169ee48a5663d084391524e57b5ff3
Nelson, Ben(Contractor) <bgnelson99@schnucks.com>
2021-01-24 21:22:52 -0600

    Added bash and shell folders. Moved bash_profile settings to these folders. Cleared out bashr file. Added comments

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,5 +1,4 @@
-#Colors
-
+# Colors
 # Normal Coloring
 txtblk='\e[0;30m' # Black - Regular
 txtred='\e[0;31m' # Red
@@ -41,34 +40,10 @@ bakcyn='\e[46m'   # Cyan
 bakwht='\e[47m'   # White
 txtrst='\e[0m'    # Text Reset
 
-#Prompt
-
+# Prompt
 print_before_the_prompt () {
 	printf "\n$bldgrn%s $bldpur%s\n$txtrst" "$PWD"
 }
 
 PROMPT_COMMAND=print_before_the_prompt
 export PS1="-> "
-
-#Aliases
-
-#ssh
-alias saturn="ssh bgnelson99@saturn"
-alias saturnlab="ssh bgnelson99@saturnlab"
-alias titan="ssh bgnelson99@titan"
-alias titanlab="ssh bgnelson99@titanlab"
-
-#color
-alias ls="ls -l -G"
-alias grep='grep --color'
-
-#cd shortcuts
-alias dl="cd ~/Downloads"
-alias dk="cd ~/Desktop"
-alias docs="cd ~/Documents"
-alias ..="cd .."
-alias home="cd ~"
-
-#ls
-alias lrth="ls -lrth"
-alias lrtha="ls -lrtha"
---
+++ shell/aliases.sh
@@ -0,0 +1,20 @@
+# ssh
+alias saturn="ssh bgnelson99@saturn"
+alias saturnlab="ssh bgnelson99@saturnlab"
+alias titan="ssh bgnelson99@titan"
+alias titanlab="ssh bgnelson99@titanlab"
+
+# color
+alias ls="ls -l -G"
+alias grep='grep --color'
+
+# cd shortcuts
+alias dl="cd ~/Downloads"
+alias dk="cd ~/Desktop"
+alias docs="cd ~/Documents"
+alias ..="cd .."
+alias home="cd ~"
+
+# ls
+alias lrth="ls -lrth"
+alias lrtha="ls -lrtha"
