aace6ea512f7b9af4f9d904a70331272fa16772d
Ben Nelson <bgnelson25@gmail.com>
2021-01-31 22:14:04 -0600

    Added tmux configuration, added install.sh file, removed unnecessary hammerspoon files, and updated the readme.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "source ~/dotfiles/vimrc" > ~/.vimrc
+echo "source ~/dotfiles/bash_profile" > ~/.bash_profile
+echo "source ~/dotfiles/bashrc" > ~/.bashrc
+echo "source ~/dotfiles/zshrc" > ~/.zshrc
+echo "source ~/dotfiles/zshrc" > ~/.zshrc
