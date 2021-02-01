71e2d8e96e73fa5765449396a02630a8e3b6c16f
Nelson, Ben(Contractor) <bgnelson99@schnucks.com>
2021-02-01 09:51:06 -0600

    Fixed incorrect install file. It now correctly installs tmux.conf.

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ echo "source ~/dotfiles/vimrc" > ~/.vimrc
 echo "source ~/dotfiles/bash_profile" > ~/.bash_profile
 echo "source ~/dotfiles/bashrc" > ~/.bashrc
 echo "source ~/dotfiles/zshrc" > ~/.zshrc
-echo "source ~/dotfiles/zshrc" > ~/.zshrc
+echo "source ~/dotfiles/tmux.conf" > ~/.tmux.conf
