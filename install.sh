e086233605e770bee8c8e504ab292f43e8ca4c06
Ben Nelson <bgnelson25@gmail.com>
2021-02-03 20:41:03 +0000

    Added beginning and ending text to install shell file.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Starting install..."
 echo "source ~/dotfiles/vimrc" > ~/.vimrc
 echo "source ~/dotfiles/bash_profile" > ~/.bash_profile
 echo "source ~/dotfiles/bashrc" > ~/.bashrc
 echo "source ~/dotfiles/zshrc" > ~/.zshrc
 echo "source ~/dotfiles/tmux.conf" > ~/.tmux.conf
+echo "Install finished!"
