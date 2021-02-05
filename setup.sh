d07f913d037c0f0cfbd1ecc19c1fa6cf5cf37b96
Ben Nelson <bgnelson25@gmail.com>
2021-02-05 00:30:57 -0600

    Added additional commands for YCM.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Compile YCM
+brew remove vim
+brew cleanup
+brew install vim
+brew install cmake python mono go nodejs
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
